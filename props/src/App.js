2e5dfd2a1777ba4441f6fbc88a64f4d6e6dc170c
jjcorbacho <jjcorbacho@gmail.com>
2021-11-14 18:17:05 -0500

    props

---
+++ props/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Saludo from './components/Saludo.jsx'
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <p>Hola mundo</p>
+    <Saludo persona='julio'/>
+    </div>
+  );
+}
+
+export default App;
